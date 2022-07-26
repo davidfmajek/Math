678791f2a05569d52a670c86ebaf24d11a18c6d5
davidfmajek <87044724+davidfmajek@users.noreply.github.com>
2022-07-26 18:11:50 -0400

    Create cm_in.py

---
+++ cm_in.py
@@ -0,0 +1,7 @@
+# centimeter to inches
+cm_ = input('Enter measurement in centimeter:')
+cm = float(cm_)
+
+#conversion ratio
+cm_in = cm/2.54
+print(cm_,'cm converted to inches is', cm_in,"inches")
